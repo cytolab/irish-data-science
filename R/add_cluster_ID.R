bd0ba13d47cd73bc144c130d8eb710463f6e35f7
Sierra Barone <sierrabarone@MacBook-Pro.local>
2021-03-12 11:42:47 -0600

    Changed install of mem

--- R/add_cluster_ID.R
+++
@@ -1,8 +0,0 @@
-addclusterID <- function(pop_list, names) {
-    # Get position in list
-    clusterID = as.numeric(names)
-    cluster = rep.int(clusterID, nrow(pop_list))
-
-    newMatrix = cbind(pop_list, cluster)
-    return(newMatrix)
-}
